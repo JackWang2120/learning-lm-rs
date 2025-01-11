562f902997e7739982f62475e3fcf96701f3d9a4
JackWang2120 <JackWang2120@gmail.com>
2025-01-11 17:20:25 +0800

    test remote

--- src/operators.rs
+++ src/operators.rs
@@ -71,18 +71,7 @@ pub fn masked_softmax(y: &mut Tensor<f32>) {
 }
 
 pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: f32) {
-    let len = y.size();
-    assert!(len == x.size());
-    let _y = unsafe { y.data_mut() };
-    let _x = x.data();
-    let _w = w.data();
-    let mean_square = _x.iter().map(|&v| v * v).sum::<f32>() / len as f32;
-    let rms = (mean_square + epsilon).sqrt();
-    println!("len of y: {}",len);
-    for i in 0..len {
-        _y[i] = _x[i] / rms * _w[i % _w.len()];
-        println!("_y {}: {}",i,_y[i]);
-    }
+   
 }
 // y = silu(x) * y
 // hint: this is an element-wise operation
